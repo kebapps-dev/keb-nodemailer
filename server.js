98c5cc5af22cc4bc47a039f0b46f740a00e8b790
Conner Glaser <conner.glaser@kebamerica.com>
2026-02-03 13:50:23 -0600

    updated to no security

--- server.js
+++ server.js
@@ -12,12 +12,8 @@ app.get('/', (_req, res) => {
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'kebamerica-com.mail.protection.outlook.com',
-  port: Number(process.env.SMTP_PORT || 25),
+  port: 25,
   secure: false, // true only for 465
-  auth: {
-    user: process.env.SMTP_USER || '',
-    pass: process.env.SMTP_PASS || '',
-  },
   tls: {
     rejectUnauthorized: false
   },
