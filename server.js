1002ab0608f49e7d9dca38cd9000107387dbce83
Conner Glaser <conner.glaser@kebamerica.com>
2026-02-05 13:39:28 -0600

    test autopush

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//options are set in environment variables or default values are used
+//options are set in environment variables or default values used
 //SMTP_HOST, MAIL_FROM, PORT (should be 4000 which is default)
 
 const express = require('express');
@@ -43,7 +43,7 @@ app.post('/api/send-email', async (req, res) => {
         encoding: 'base64'
       }));
     }
-    
+
     console.log('Email sent:', info.messageId);
     res.json({ success: true });
   } catch (error) {
